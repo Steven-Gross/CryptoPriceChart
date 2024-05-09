f4fdaf7e6d24a84d59a0beb8f1fb9e814823eff3
grossatron <sgross1012@gmail.com>
2024-05-09 08:20:39 -0400

    updated index.html and main.js to display the first three cryptocurrencies available on coincap api

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@ const url = 'https://api.coincap.io/v2/assets'
 fetch(url)
   .then(res => res.json()) // parse response as JSON
   .then(data => {
-    let btcPrice = Math.round(data.data[0].priceUsd)
-    let ethPrice = Math.round(data.data[1].priceUsd)
-    document.querySelector('.bitcoin').innerText = `BTC $${btcPrice}`
-    document.querySelector('.ethereum').innerText = `ETH $${ethPrice}`
-
-
-
+    let currency0Price = Math.round(data.data[0].priceUsd)
+    let currency0Symbol = data.data[0].symbol
+    let currency1Price = Math.round(data.data[1].priceUsd)
+    let currency1Symbol = data.data[1].symbol
+    let currency2Price = Math.round(data.data[2].priceUsd)
+    let currency2Symbol = data.data[2].symbol
+    document.querySelector('.currency0').innerText = `${currency0Symbol} $${currency0Price}`
+    document.querySelector('.currency1').innerText = `${currency1Symbol} $${currency1Price}`
+    document.querySelector('.currency2').innerText = `${currency2Symbol} $${currency2Price}`
     console.log(data)
 
   })
